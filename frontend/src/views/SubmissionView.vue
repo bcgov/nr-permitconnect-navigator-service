ff2bae0b7e775638b4725cc637a6022d664496f0
Kyle Morel <kyle.1.morel@gov.bc.ca>
2024-01-08 18:44:01 -0800

    FileUpload and Carousel components

--- frontend/src/views/SubmissionView.vue
+++ frontend/src/views/SubmissionView.vue
@@ -2,9 +2,12 @@
 import { storeToRefs } from 'pinia';
 import { onMounted, ref } from 'vue';
 
-import SubmissionForm from '../components/submission/SubmissionForm.vue';
-import { Button, useToast } from '@/lib/primevue';
-import { chefsService, documentService } from '@/services';
+import FileUpload from '@/components/file/FileUpload.vue';
+import SubmissionForm from '@/components/submission/SubmissionForm.vue';
+import { Button, Carousel, TabPanel, TabView, useToast } from '@/lib/primevue';
+import { chefsService } from '@/services';
+import { useFileStore } from '@/store';
+import { formatDateLong } from '@/utils/formatters';
 import { useConfigStore } from '@/store';
 
 import type { Ref } from 'vue';
@@ -23,6 +26,8 @@ const { getConfig } = storeToRefs(useConfigStore());
 // State
 const editable: Ref<boolean> = ref(false);
 const submission: Ref<any | undefined> = ref(undefined);
+
+const { getFiles } = storeToRefs(useFileStore());
 
 // Actions
 const toast = useToast();
@@ -52,18 +57,77 @@ const onUpload = async (file: File) => {
 <template>
   <h1>Activity submission</h1>
 
-  <Button
-    class="mb-3"
-    @click="editable = !editable"
-  >
-    Edit
-  </Button>
+  <TabView>
+    <TabPanel header="Info">
+      <Button
+        class="mb-3"
+        @click="editable = !editable"
+      >
+        Edit
+      </Button>
 
-  <SubmissionForm
-    v-if="submission"
-    :editable="editable"
-    :submission="submission"
-    @cancel="onCancel"
-    @submit="onSubmit"
-  />
+      <SubmissionForm
+        v-if="submission"
+        :editable="editable"
+        :submission="submission"
+        @cancel="onCancel"
+        @submit="onSubmit"
+      />
+    </TabPanel>
+    <TabPanel header="Files">
+      <div class="grid border-1 surface-border border-round text-center">
+        <div class="col-2">
+          <FileUpload />
+        </div>
+        <div class="col-10 p-0">
+          <Carousel
+            v-if="getFiles.length > 0"
+            :value="getFiles"
+            :num-visible="3"
+            :num-scroll="3"
+          >
+            <template #item="slotProps">
+              <div class="grid border-1 surface-border border-round text-center m-2">
+                <div class="col-12 text-center">
+                  <img
+                    src="../assets/images/bcboxy.png"
+                    class="carousel-image"
+                  />
+                </div>
+                <h4 class="col-12 mb-0 text-left">{{ slotProps.data.name }}</h4>
+
+                <h6 class="col-8 text-left mt-0">
+                  {{ formatDateLong(slotProps.data.lastModifiedDate.toISOString()) }}
+                </h6>
+                <h6 class="col-4 text-right mt-0">{{ slotProps.data.size }}</h6>
+              </div>
+            </template>
+          </Carousel>
+        </div>
+      </div>
+    </TabPanel>
+  </TabView>
 </template>
+
+<style lang="scss">
+.carousel-image {
+  max-height: 140px;
+  width: auto;
+  height: auto;
+}
+
+.p-carousel-item {
+  height: 270px;
+}
+
+.p-tabview {
+  .p-tabview-nav-container {
+    padding-bottom: 2rem;
+  }
+
+  .p-tabview-title {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+}
+</style>
