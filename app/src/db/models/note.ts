794c823023b6ca483814469c0deb8dd6584d9f4e
Wilson Wong <wilwong89@gmail.com>
2024-03-27 10:13:47 -0700

    Note model and functionality changes

--- app/src/db/models/note.ts
+++ app/src/db/models/note.ts
@@ -14,6 +14,8 @@ export default {
     return {
       note_id: input.noteId,
       activity_id: input.activityId,
+      bringForwardDate: input.bringForwardDate ? new Date(input.bringForwardDate) : null,
+      bringForwardState: input.bringForwardState,
       note: input.note,
       note_type: input.noteType,
       title: input.title,
@@ -30,6 +32,8 @@ export default {
     return {
       noteId: input.note_id,
       activityId: input.activity_id,
+      bringForwardDate: input.bringForwardDate?.toISOString() ?? null,
+      bringForwardState: input.bringForwardState || '',
       note: input.note || '',
       noteType: input.note_type || '',
       title: input.title || '',
