1f7964435527c9d20e797815451151c2dbd45623
Kyle Morel <kyle.1.morel@gov.bc.ca>
2024-04-23 16:22:35 -0700

    Finalized styling. Added custom Stepper header and navigation components

---
+++ frontend/src/components/form/StepperHeader.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Button } from '@/lib/primevue';
+
+// Props
+type Props = {
+  index: number;
+  activeStep: number;
+  clickCallback?: Function;
+  title: string;
+  icon: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  clickCallback: () => {}
+});
+</script>
+
+<template>
+  <Button
+    class="p-button-text"
+    @click="props.clickCallback()"
+  >
+    <font-awesome-icon
+      class="pr-2"
+      :icon="`fa-solid ${props.icon}`"
+    />
+    <span
+      class="text-xl"
+      :class="{ 'font-bold': props.index === props.activeStep }"
+    >
+      {{ props.title }}
+    </span>
+  </Button>
+</template>
