c5f005f90e71723a298e05a263c95fd12db38466
Kyle Morel <kyle.1.morel@gov.bc.ca>
2024-06-03 14:40:51 -0700

    Form text and alignment updates

--- frontend/src/views/housing/HousingView.vue
+++ frontend/src/views/housing/HousingView.vue
@@ -31,65 +31,78 @@ const items = ref([
 </script>
 
 <template>
-  <div class="flex flex-row align-items-center w-full housing">
-    <font-awesome-icon
-      icon="fa-solid fa-house"
-      class="mr-3 text-5xl app-primary-color"
-    />
-    <h1 class="app-primary-color">Housing</h1>
-  </div>
-  <div class="flex flex-column">
-    <div
-      v-for="(item, index) in items"
-      :key="index"
-      class="flex justify-content-center mb-5"
-    >
-      <router-link
-        v-slot="{ href, navigate }"
-        :to="{ name: item.route }"
-        custom
+  <div class="flex flex-column align-items-center justify-content-start h-full">
+    <div class="flex flex-row align-items-center w-full housing">
+      <img
+        class="mr-3"
+        src="@/assets/images/house.png"
+        width="147"
+        height="141"
+        alt="Housing image"
+      />
+      <h1 class="app-primary-color">Housing</h1>
+    </div>
+    <div class="flex flex-column align-items-center justify-content-start mt-8">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="flex justify-content-center mb-5"
       >
-        <a
-          :href="href"
-          @click="navigate"
+        <router-link
+          v-slot="{ href, navigate }"
+          :to="{ name: item.route }"
+          custom
         >
-          <Button
-            class="p-0 m-0"
-            text
+          <a
+            :href="href"
+            @click="navigate"
           >
-            <div class="card">
-              <div class="flex align-items-center text-left h-full mx-3">
-                <div :class="['mr-2', { 'pb-4': item.bottom }]">
-                  <font-awesome-icon
-                    :icon="`fa-solid
+            <Button
+              class="p-0 m-0"
+              text
+            >
+              <div class="card">
+                <div class="flex align-items-center text-left h-full mx-3">
+                  <div :class="['mr-2 text-2xl', { 'pb-4': item.bottom }]">
+                    <font-awesome-icon
+                      :icon="`fa-solid
                   ${item.icon}`"
-                  />
-                </div>
-                <div>
-                  <div class="font-bold">{{ item.top }}</div>
-                  <div v-if="item.bottom">{{ item.bottom }}</div>
+                    />
+                  </div>
+                  <div>
+                    <div class="font-bold">{{ item.top }}</div>
+                    <div v-if="item.bottom">{{ item.bottom }}</div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Button>
-        </a>
-      </router-link>
+            </Button>
+          </a>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.card {
+.p-button {
   border-width: 2px;
   border-style: solid;
   border-radius: 0.5rem;
   border-color: $app-primary;
   width: 550px;
   height: 80px;
+  background-color: #fff;
+  color: $app-primary;
+  box-shadow: 0 6px 6px -1px rgb(145, 145, 145);
+}
+
+.p-button:hover {
+  opacity: 1;
+  background-color: $app-primary;
+  color: $app-outline-on-primary;
 }
 
 .housing {
-  // background-color: #f8f9fa;
   padding-left: 3.5rem;
 }
 </style>
