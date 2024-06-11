b70cba5534dc8153bcd5d9a18228020ee68f75d0
Wilson Wong <wilwong89@gmail.com>
2024-06-11 09:01:53 -0700

    Import components, restructure form

--- frontend/src/components/housing/enquiry/EnquiryEditForm.vue
+++ frontend/src/components/housing/enquiry/EnquiryEditForm.vue
@@ -4,7 +4,10 @@ import { Form } from 'vee-validate';
 import { onBeforeMount, ref, toRaw } from 'vue';
 
 import { Dropdown, EditableDropdown, InputMask, RadioList, InputText, TextArea } from '@/components/form';
-import { submissionService, userService } from '@/services';
+import { Button, useConfirm, useToast } from '@/lib/primevue';
+import { enquiryService, submissionService, userService } from '@/services';
+import { useEnquiryStore } from '@/store';
+
 import {
   ApplicationStatusList,
   ContactPreferenceList,
@@ -17,17 +20,18 @@ import {
 import type { Ref } from 'vue';
 import type { IInputEvent } from '@/interfaces';
 import type { Submission, User } from '@/types';
+import { CONTACT_PREFERENCE_LIST } from '@/utils/enums';
 
 // Props
-// type Props = {
-//   activityId?: string;
-//   enquiryId?: string;
-// };
+type Props = {
+  activityId?: string;
+  enquiryId?: string;
+};
 
-// const props = withDefaults(defineProps<Props>(), {
-//   activityId: undefined,
-//   enquiryId: undefined
-// });
+const props = withDefaults(defineProps<Props>(), {
+  activityId: undefined,
+  enquiryId: undefined
+});
 
 // State
 const assigneeOptions: Ref<Array<User>> = ref([]);
@@ -35,9 +39,20 @@ const editable: Ref<boolean> = ref(true);
 const initialFormValues: Ref<any | undefined> = ref(undefined);
 
 // Actions
-// const confirm = useConfirm();
+const confirm = useConfirm();
+const enquiryStore = useEnquiryStore();
 // const router = useRouter();
-// const toast = useToast();
+const toast = useToast();
+
+const confirmSubmit = (data: any) => {
+  confirm.require({
+    message: 'Are you sure you wish to submit this form? Please review the form before submitting.',
+    header: 'Please confirm submission',
+    acceptLabel: 'Confirm',
+    rejectLabel: 'Cancel',
+    accept: () => onSubmit(data)
+  });
+};
 
 const getAssigneeOptionLabel = (e: User) => {
   return `${e.fullName} [${e.email}]`;
@@ -55,75 +70,98 @@ const onAssigneeInput = async (e: IInputEvent) => {
   }
 };
 
+const onSubmit = async (data: any) => {
+  editable.value = false;
+  try {
+    if (data.enquiryId) {
+      await enquiryService.updateDraft(data.enquiryId, { ...data, submit: true });
+    } else {
+      await enquiryService.createDraft({ ...data, submit: true });
+    }
+  } catch (e: any) {
+    toast.error('Failed to save intake', e);
+  } finally {
+    editable.value = true;
+  }
+};
+
 onBeforeMount(async () => {
   // assigneeOptions.value = (await userService.searchUsers({ userId: [props.submission.assignedUserId] })).data;
+  initialFormValues.value = enquiryStore.getEnquiry;
 });
 </script>
 
 <template>
-  <Form>
-    <div class="flex formgrid grid">
+  <Form
+    v-slot="{ handleReset }"
+    :validation-schema="{}"
+    :initial-values="initialFormValues"
+    @submit="confirmSubmit"
+  >
+    <div class="formgrid grid">
       <InputText
         class="col-3"
-        name="activity"
+        name="activityId"
         label="Activity"
         :bold="false"
-        :disabled="!editable"
+        :disabled="true"
       />
-      <InputText
+      <Dropdown
         class="col-3"
         name="enquiryType"
         label="Enquiry Type"
         :bold="false"
         :disabled="!editable"
+        :options="[]"
       />
       <InputText
         class="col-3"
-        name="Submission date"
-        label="First name"
+        name="contactFirstName"
+        label="Contact name"
         :bold="false"
         :disabled="!editable"
       />
       <InputText
         class="col-3"
-        name="relatedSubmission"
+        name="relatedActivityId"
         label="Related submission"
         :bold="false"
-        :disabled="!editable"
+        :disabled="true"
       />
 
       <InputText
         class="col-3"
-        name="firstName"
+        name="contactFirstName"
         label="Contact"
         :bold="false"
         :disabled="!editable"
       />
       <InputText
         class="col-3"
-        name="phoneNumber"
+        name="contactPhoneNumber"
         label="Enquiry Type"
         :bold="false"
         :disabled="!editable"
       />
       <InputText
         class="col-3"
-        name="email"
+        name="contactEmail"
         label="First name"
         :bold="false"
         :disabled="!editable"
       />
-      <InputText
+      <Dropdown
         class="col-3"
         name="contactPreference"
-        label="Related submission"
+        label="Preferred contact method"
         :bold="false"
         :disabled="!editable"
+        :options="ContactPreferenceList"
       />
       <InputText
         class="col-6"
         name="relationshipToProject"
-        label="Related submission"
+        label="Relationship to activity"
         :bold="false"
         :disabled="!editable"
       />
@@ -163,6 +201,29 @@ onBeforeMount(async () => {
         label="Activity state"
         :disabled="!editable"
         :options="ApplicationStatusList"
+      />
+    </div>
+    <div
+      v-if="editable"
+      class="field col-12"
+    >
+      <Button
+        label="Save"
+        type="submit"
+        icon="pi pi-check"
+        :disabled="!editable"
+      />
+      <Button
+        label="Cancel"
+        outlined
+        class="ml-2"
+        icon="pi pi-times"
+        :disabled="!editable"
+        @click="
+          () => {
+            handleReset();
+          }
+        "
       />
     </div>
   </Form>
