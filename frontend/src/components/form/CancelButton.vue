02f72ab5a68b6c0fb593e1c624108e4afa435822
Kyle Morel <kyle.1.morel@gov.bc.ca>
2024-06-17 13:35:17 -0700

    Add CancelButton and FormNavigationGuard components

---
+++ frontend/src/components/form/CancelButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useIsFormDirty } from 'vee-validate';
+
+import { Button } from '@/lib/primevue';
+
+// Props
+type Props = {
+  editable?: boolean;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  editable: true
+});
+
+// Emits
+const emit = defineEmits(['clicked']);
+
+// State
+const isDirty = useIsFormDirty();
+</script>
+
+<template>
+  <Button
+    label="Cancel"
+    outlined
+    class="ml-2 p-button-danger"
+    icon="pi pi-times"
+    :disabled="!props.editable || !isDirty"
+    @click="emit('clicked')"
+  />
+</template>
---
+++ frontend/src/components/form/FormNavigationGuard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useConfirm } from '@/lib/primevue';
+import { onBeforeRouteLeave, useRouter } from 'vue-router';
+import { useIsFormDirty } from 'vee-validate';
+import { ref } from 'vue';
+
+import type { Ref } from 'vue';
+
+// State
+const isDirty = useIsFormDirty();
+const isOpen: Ref<boolean> = ref(false);
+
+// Actions
+const confirm = useConfirm();
+const router = useRouter();
+
+onBeforeRouteLeave((to) => {
+  if (isDirty.value && !isOpen.value) {
+    isOpen.value = true;
+    confirm.require({
+      message: 'Are you sure you want to leave this page? Any unsaved changes will be lost.',
+      header: 'Leave this page?',
+      acceptLabel: 'Leave',
+      acceptClass: 'p-button-danger',
+      rejectLabel: 'Cancel',
+      accept: () => router.replace(to),
+      reject: () => (isOpen.value = false)
+    });
+
+    return false;
+  }
+
+  return true;
+});
+</script>
