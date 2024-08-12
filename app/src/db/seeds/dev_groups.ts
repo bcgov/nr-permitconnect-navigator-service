6df4ceac3f30f92d01ac822e7b9b51b3368df993
Kyle Morel <kyle.1.morel@gov.bc.ca>
2024-08-12 11:31:38 -0700

    Update pr workflow to apply db group seeding

--- app/src/db/seeds/dev_groups.ts
+++ app/src/db/seeds/dev_groups.ts
@@ -12,8 +12,7 @@ export async function seed(knex: Knex): Promise<void> {
     .where({ initiative_id: pcns_id, name: GroupName.DEVELOPER })
     .select('group_id');
 
-  // Inserts seed entries
-  await knex('yars.identity_group').insert([
+  const identities = [
     { identity_id: '8895e3992ee9455b8eef3ab435dd5486@idir', group_id: developer_group_id[0].group_id },
     { identity_id: '17222a61118846b08bdda2769a585278@idir', group_id: developer_group_id[0].group_id },
     { identity_id: 'a64c23b15d964d9b979188f356e083ea@idir', group_id: developer_group_id[0].group_id },
@@ -22,5 +21,18 @@ export async function seed(knex: Knex): Promise<void> {
     { identity_id: 'e7d30762682b437cb8d60502c956a516@idir', group_id: developer_group_id[0].group_id },
     { identity_id: 'ff3ce3f9a6664dfd82a303a189411446@idir', group_id: developer_group_id[0].group_id },
     { identity_id: '4a07a001034549d79f525715378e617a@idir', group_id: developer_group_id[0].group_id }
-  ]);
+  ];
+
+  const exists = await knex('yars.identity_group').whereIn(
+    'identity_id',
+    identities.map((x) => x.identity_id)
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const filtered = identities.filter((x) => !exists.some((y: any) => y.identity_id === x.identity_id));
+
+  // Inserts missing seed entries
+  if (filtered.length > 0) {
+    await knex('yars.identity_group').insert(filtered);
+  }
 }
