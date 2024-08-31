591b698fc1c89effc3872297caa92b7c59c070a2
Wilson Wong <wilwong89@gmail.com>
2024-08-30 18:08:09 -0700

    Fix column names, add comments to migration

--- app/src/db/migrations/20240828000000_008-user-id-constraint.ts
+++ app/src/db/migrations/20240828000000_008-user-id-constraint.ts
@@ -4,24 +4,25 @@ import type { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return (
     Promise.resolve()
-      // Drop public schema functions
+      // Find all duplicated user entries in "user" table (by querying by "identity_id")
+      // Then delete the most recent duplicated entry
       .then(() =>
         knex.raw(`DELETE FROM "public"."user" AS u
-        WHERE u."userId" IN (
-          SELECT DISTINCT ON ("public"."user"."identityId") "public"."user"."userId"
+        WHERE u."user_id" IN (
+          SELECT DISTINCT ON ("public"."user"."identity_id") "public"."user"."user_id"
           FROM "public"."user"
           WHERE (
             SELECT count(*)
             FROM "public"."user" AS user2
-            WHERE user2."identityId" = "public"."user"."identityId") > 1
-          ORDER BY "public"."user"."identityId", "public"."user"."createdAt" DESC
+            WHERE user2."identity_id" = "public"."user"."identity_id") > 1
+          ORDER BY "public"."user"."identity_id", "public"."user"."created_at" DESC
         )`)
       )
 
       // prevent further duplicate users - add unique index over columns identityId and idp in user table
       .then(() =>
         knex.schema.alterTable('user', (table) => {
-          table.unique(['identityId', 'idp']);
+          table.unique(['identity_id', 'idp']);
         })
       )
   );
@@ -30,7 +31,7 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
   return Promise.resolve().then(() =>
     knex.schema.alterTable('user', (table) => {
-      table.dropUnique(['identityId', 'idp']);
+      table.dropUnique(['identity_id', 'idp']);
     })
   );
 }
