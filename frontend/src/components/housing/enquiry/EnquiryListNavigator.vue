2c98d501bf7270620063dbb0e63431187a6ca6a0
Sanjay Babu <sanjaytkbabu@gmail.com>
2024-09-19 15:57:48 -0700

    preserve pagination for enquiry

--- frontend/src/components/housing/enquiry/EnquiryListNavigator.vue
+++ frontend/src/components/housing/enquiry/EnquiryListNavigator.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 import { Spinner } from '@/components/layout';
 import {
@@ -17,9 +18,18 @@ import { enquiryService } from '@/services';
 import { RouteName } from '@/utils/enums/application';
 import { IntakeStatus } from '@/utils/enums/housing';
 import { formatDate } from '@/utils/formatters';
+import { toNumber } from '@/utils/utils';
 
 import type { Ref } from 'vue';
 import type { Enquiry } from '@/types';
+
+// Types
+type Pagination = {
+  rows?: number;
+  order?: number;
+  field?: string;
+  page?: number;
+};
 
 // Props
 const { loading, enquiries } = defineProps<{
@@ -31,11 +41,27 @@ const { loading, enquiries } = defineProps<{
 const emit = defineEmits(['enquiry:delete']);
 
 // State
+const route = useRoute();
 const selection: Ref<Enquiry | undefined> = ref(undefined);
+const pagination: Ref<Pagination> = ref({
+  rows: 10,
+  order: -1,
+  field: 'submittedAt',
+  page: 0
+});
+
+// read from query params if tab is set to enquiry otherwise use default values
+if (route.query.tab === '1') {
+  pagination.value.rows = toNumber(route.query.rows as string) ?? 10;
+  pagination.value.order = toNumber(route.query.order as string) ?? -1;
+  pagination.value.field = (route.query.field as string) ?? 'submittedAt';
+  pagination.value.page = toNumber(route.query.page as string) ?? 0;
+}
 
 // Actions
 const confirm = useConfirm();
 const toast = useToast();
+const router = useRouter();
 
 function onDelete(enquiryId: string, activityId: string) {
   confirm.require({
@@ -61,6 +87,20 @@ function onDelete(enquiryId: string, activityId: string) {
 const filters = ref({
   global: { value: null, matchMode: FilterMatchMode.CONTAINS }
 });
+
+// Actions
+function updateQueryParams() {
+  router.replace({
+    name: RouteName.HOUSING_SUBMISSIONS,
+    query: {
+      rows: pagination.value.rows ?? undefined,
+      order: pagination.value.order ?? undefined,
+      field: pagination.value.field ?? undefined,
+      page: pagination.value.page ?? undefined,
+      tab: route.query.tab ?? 1
+    }
+  });
+}
 </script>
 
 <template>
@@ -73,13 +113,35 @@ const filters = ref({
     scrollable
     responsive-layout="scroll"
     :paginator="true"
-    :rows="10"
-    sort-field="submittedAt"
-    :sort-order="-1"
+    :rows="pagination.rows"
+    :sort-field="pagination.field"
+    :sort-order="pagination.order"
     paginator-template="RowsPerPageDropdown CurrentPageReport PrevPageLink NextPageLink "
     current-page-report-template="{first}-{last} of {totalRecords}"
     :rows-per-page-options="[10, 20, 50]"
     selection-mode="single"
+    :first="pagination.page && pagination.rows ? pagination.page * pagination.rows : 0"
+    @update:sort-field="
+      (e) => {
+        pagination.field = e;
+        pagination.page = 0;
+        updateQueryParams();
+      }
+    "
+    @update:sort-order="
+      (e) => {
+        pagination.order = e ?? -1;
+        pagination.page = 0;
+        updateQueryParams();
+      }
+    "
+    @page="
+      (e) => {
+        pagination.page = e.page;
+        pagination.rows = e.rows;
+        updateQueryParams();
+      }
+    "
   >
     <template #empty>
       <div class="flex justify-content-center">
