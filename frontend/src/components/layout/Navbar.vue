c8d029438a053a8307b29925d123753510ca42ab
Wilson Wong <wilwong89@gmail.com>
2024-10-11 23:58:08 -0700

    WIP menu styling

--- frontend/src/components/layout/Navbar.vue
+++ frontend/src/components/layout/Navbar.vue
@@ -100,10 +100,16 @@ onMounted(() => {
 </script>
 
 <template>
-  <nav class="navigation-main pl-2 lg:pl-6">
-    <Menubar :model="items">
+  <nav class="navigation-main">
+    <Menubar
+      :model="items"
+      class="pl-6"
+    >
       <template #item="{ item, props, hasSubmenu }">
-        <span v-if="item.public || authzStore.canNavigate(item.access)">
+        <span
+          v-if="item.public || authzStore.canNavigate(item.access)"
+          class="menu-item"
+        >
           <router-link
             v-if="item.route"
             v-slot="{ href, navigate }"
@@ -158,99 +164,7 @@ onMounted(() => {
 </template>
 
 <style lang="scss" scoped>
-.navigation-main {
-  background-color: $app-primary;
+:deep(.p-menubar-item-link) {
+  padding: 0.5rem 0.4rem 0.7rem;
 }
-// .navigation-main {
-//   background-color: #38598a;
-//   color: #fcba19;
-//   display: flex;
-//   width: 100%;
-//   box-shadow: 0 6px 8px -4px #b3b1b3;
-
-//   .p-menubar {
-//     border: none;
-//     padding: 0;
-//     background-color: $app-link-text;
-
-//     :deep(.p-menuitem) {
-//       &.p-focus {
-//         > .p-menuitem-content {
-//           background-color: #5a7da9;
-//         }
-//       }
-//     }
-
-//     :deep(.p-menuitem-content) {
-//       background-color: #38598a;
-//       border-radius: 0;
-//       padding: 0;
-//     }
-
-//     :deep(.p-submenu-list) {
-//       border-radius: 0;
-//       padding: 0;
-//     }
-
-//     :deep(.p-menuitem-link) {
-//       padding: 0.5rem 0.8rem 0.7rem 0.8rem;
-
-//       &:hover {
-//         background-color: #5a7da9;
-//       }
-//     }
-
-//     :deep(.p-menuitem.p-highlight > .p-menuitem-content) {
-//       background-color: #5a7da9;
-//     }
-
-//     :deep(.p-menuitem .p-focus) {
-//       background-color: #38598a;
-//       border-radius: 0;
-//     }
-
-//     .mail-link {
-//       padding: 0.5rem 0.8rem 0.7rem 0.8rem;
-//       &:hover {
-//         background-color: #5a7da9;
-//       }
-//     }
-
-//     ul {
-//       display: flex;
-//       flex-direction: row;
-//       margin: 0;
-//       color: #ffffff;
-//       list-style: none;
-
-//       li {
-//         margin-right: 1em;
-
-//         a,
-//         p {
-//           display: flex;
-//           font-weight: normal;
-//           min-height: 2rem;
-//           color: #ffffff;
-//           padding: 0;
-//           margin: 0;
-//           text-decoration: none;
-//         }
-//       }
-//     }
-
-//     :deep(.p-submenu-list) {
-//       background-color: #38598a !important;
-//       width: 250px;
-
-//       .p-menuitem:first-child {
-//         border-top: 2px solid #fcba19;
-//       }
-//     }
-
-//     :deep(.pi) {
-//       color: #ffffff;
-//     }
-//   }
-// }
 </style>
