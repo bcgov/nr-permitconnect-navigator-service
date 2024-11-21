135189fd93b82062968d05c9f63120b60cc7bcac
qhanson55 <quinnhanson55@gmail.com>
2024-11-21 14:05:00 -0800

    Add permit notes to db; migration, models, and types

---
+++ app/src/validators/permitNote.ts
@@ -0,0 +1,43 @@
+import Joi from 'joi';
+
+import { uuidv4 } from './common';
+import { validate } from '../middleware/validation';
+
+const sharedPermitNoteSchema = {
+  permitId: uuidv4.required(),
+  note: Joi.string().required(),
+  isDeleted: Joi.boolean()
+};
+
+const schema = {
+  createPermitNote: {
+    body: Joi.object(sharedPermitNoteSchema)
+  },
+  listPermitNotes: {
+    params: Joi.object({
+      permitId: uuidv4.required()
+    })
+  }
+  // TODO impliment update & delete validators
+  // updatePermitNote: {
+  //   body: Joi.object({
+  //     ...sharedPermitSchema,
+  //     permitNoteId: uuidv4.required()
+  //   }),
+  //   query: Joi.object({
+  //     permitId: uuidv4.required()
+  //   })
+  // },
+  // deletePermitNote: {
+  //   query: Joi.object({
+  //     permitId: uuidv4.required()
+  //   })
+  // },
+};
+
+export default {
+  createPermitNote: validate(schema.createPermitNote),
+  listPermitNotes: validate(schema.listPermitNotes)
+  // deletePermit: validate(schema.deletePermit),
+  // updatePermit: validate(schema.updatePermit)
+};
