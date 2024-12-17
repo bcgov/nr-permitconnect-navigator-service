d1c4638510401c7b6b3c7279e19d3810ad538918
Kyle Morel <kyle.1.morel@gov.bc.ca>
2024-12-17 09:02:51 -0800

    Stepper styling changes

--- frontend/src/components/form/StepperHeader.vue
+++ frontend/src/components/form/StepperHeader.vue
@@ -18,19 +18,40 @@ const {
 </script>
 
 <template>
-  <Button
-    class="p-button-text"
-    @click="clickCallback()"
-  >
-    <font-awesome-icon
-      class="pr-2"
-      :icon="`fa-solid ${icon}`"
-    />
+  <div class="flex flex-column align-items-center">
+    <Button
+      class="bg-transparent border-none inline-flex flex-column gap-2 p-1"
+      :class="[{ 'outer-border': index === activeStep }]"
+      @click="clickCallback()"
+    >
+      <span
+        class="border-circle border-2 w-3rem h-3rem inline-flex align-items-center justify-content-center"
+        :class="[
+          {
+            'bg-primary border-primary': index <= activeStep,
+            'surface-border': index > activeStep,
+            'border-1, p-4': index === activeStep
+          }
+        ]"
+      >
+        <font-awesome-icon
+          :icon="`fa-solid ${icon}`"
+          :class="[{ 'app-primary-color': index > activeStep }]"
+        />
+      </span>
+    </Button>
     <span
       class="text-xl"
-      :class="{ 'font-bold': index === activeStep }"
+      :class="{ 'font-bold': index === activeStep, underline: index === activeStep }"
     >
       {{ title }}
     </span>
-  </Button>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.outer-border {
+  border-radius: 50%;
+  box-shadow: 0 0 0 3px #c1ddfc; // TODO: Blue 30
+}
+</style>
