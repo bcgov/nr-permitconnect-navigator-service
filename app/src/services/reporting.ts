31a8f9cd9a8ae22c7e964db595eaa0d63a46cffa
qhanson55 <quinnhanson55@gmail.com>
2025-03-04 11:19:55 -0800

    Add more columns to export data

--- app/src/services/reporting.ts
+++ app/src/services/reporting.ts
@@ -3,23 +3,26 @@ import prisma from '../db/dataConnection';
 const service = {
   getSubmissionPermitData: async () => {
     const result = await prisma.$queryRaw`
-    select project_name, 
-      case 
-        when s.consent_to_feedback then 'Yes' 
-        else 'No' 
-      end as consent_to_feedback, 
-      c.first_name, 
-      c.last_name, 
-      c.phone_number, 
-      c.email, 
-      c.contact_preference, 
-      c.contact_applicant_relationship, 
-      s.activity_id, 
-      street_address, 
-      locality, 
-      latitude, 
-      longitude, 
-      location_pids, 
+    select project_name,
+      case
+        when s.consent_to_feedback then 'Yes'
+        else 'No'
+      end as consent_to_feedback,
+      c.first_name,
+      c.last_name,
+      c.phone_number,
+      c.email,
+      c.contact_preference,
+      c.contact_applicant_relationship,
+      s.activity_id,
+      street_address,
+      locality,
+      latitude,
+      longitude,
+      location_pids,
+      submission_type,
+      intake_status,
+      application_status,
       p.issued_permit_id,
       p.tracking_id,
       p.auth_status,
@@ -37,12 +40,12 @@ const service = {
       pt.acronym,
       pt.tracked_in_ats,
       pt.source_system,
-      pt.source_system_acronym 
-    from submission as s 
-    join activity as a on s.activity_id = a.activity_id 
+      pt.source_system_acronym
+    from submission as s
+    join activity as a on s.activity_id = a.activity_id
     join activity_contact as ac on s.activity_id = ac.activity_id
-    join contact as c on ac.contact_id = c.contact_id 
-    join permit as p on s.activity_id = p.activity_id 
+    join contact as c on ac.contact_id = c.contact_id
+    join permit as p on s.activity_id = p.activity_id
     join permit_type pt on p.permit_type_id = pt.permit_type_id
     where a.is_deleted = false`;
 
