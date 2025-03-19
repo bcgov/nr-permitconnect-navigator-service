f5558a1dff8f902c2a2ffb4888ace51e29d4fcac
Sanjay Babu <sanjaytkbabu@gmail.com>
2025-03-19 16:42:03 -0700

    New login flow

--- frontend/src/views/oidc/OidcLoginView.vue
+++ frontend/src/views/oidc/OidcLoginView.vue
@@ -1,29 +1,116 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { useI18n } from 'vue-i18n';
 
-import { Spinner } from '@/components/layout';
+import { Button, Card, Divider } from '@/lib/primevue';
 import { useAuthNStore } from '@/store';
+import { IdentityProviderKind } from '@/utils/enums/application';
+import { findIdpConfig } from '@/utils/utils';
 
-const authnStore = useAuthNStore();
+// Actions
+const { t } = useI18n();
 
-onMounted(() => {
-  authnStore.login();
-});
+function login(kind: IdentityProviderKind) {
+  useAuthNStore().login(findIdpConfig(kind)?.idp);
+}
 </script>
 
 <template>
-  <h2>Logging in...</h2>
-  <Spinner />
+  <div class="flex justify-center mt-7">
+    <Card class="flex items-center card">
+      <template #title>
+        <div>
+          <h3 class="mb-0 mx-2">{{ t('oidcLoginView.login') }}</h3>
+        </div>
+      </template>
+      <template #content>
+        <div class="flex flex-col justify-center mx-2">
+          <Button
+            class="mb-3"
+            data-test="bcsc-login-button"
+            :aria-label="t('oidcLoginView.loginBCServicesCard')"
+            @click="login(IdentityProviderKind.BCSC)"
+          >
+            {{ t('oidcLoginView.loginBCServicesCard') }}
+          </Button>
+          <Button
+            class="mb-3"
+            data-test="bceid-login-button"
+            :aria-label="t('oidcLoginView.loginBasicBCeID')"
+            @click="login(IdentityProviderKind.BCEID)"
+          >
+            {{ t('oidcLoginView.loginBasicBCeID') }}
+          </Button>
+          <Button
+            class="mb-3"
+            data-test="bceid-business-login-button"
+            :aria-label="t('oidcLoginView.loginBusinessBCeID')"
+            @click="login(IdentityProviderKind.BCEIDBUSINESS)"
+          >
+            {{ t('oidcLoginView.loginBusinessBCeID') }}
+          </Button>
+
+          <Divider />
+
+          <h4 class="my-3">{{ t('oidcLoginView.internalGovUsers') }}</h4>
+
+          <Button
+            class="p-button-outlined"
+            data-test="idir-login-button"
+            :aria-label="t('oidcLoginView.loginIDIR')"
+            @click="login(IdentityProviderKind.IDIR)"
+          >
+            {{ t('oidcLoginView.loginIDIR') }}
+          </Button>
+          <div class="my-3">
+            <Divider />
+          </div>
+
+          <h4 class="mb-3">{{ t('oidcLoginView.register') }}</h4>
+          <div class="account-setup p-2 mb-3">
+            <a
+              class="mb-3 mr-1"
+              href="https://id.gov.bc.ca/account/"
+              target="_blank"
+            >
+              {{ t('oidcLoginView.setupBCServicesCard') }}
+            </a>
+            <span>{{ t('oidcLoginView.BCServicesCardRequirement') }}</span>
+          </div>
+          <div class="account-setup p-2 mb-3">
+            <a
+              class="mb-3 mr-1"
+              href="https://www.bceid.ca/register/basic/account_details.aspx?type=regular&eServiceType=basic"
+              target="_blank"
+            >
+              {{ t('oidcLoginView.getBasicBCeID') }}
+            </a>
+            <span>{{ t('oidcLoginView.BCeIDRequirement') }}</span>
+          </div>
+
+          <div class="account-setup p-2">
+            <a
+              class="mb-3 mr-1"
+              href="https://www.bceid.ca/register/business/getting_started/getting_started.aspx"
+              target="_blank"
+            >
+              {{ t('oidcLoginView.getBusinessBCeID') }}
+            </a>
+            <span>{{ t('oidcLoginView.BusinessBCeIDRequirement') }}</span>
+          </div>
+        </div>
+      </template>
+    </Card>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-h2 {
-  margin-top: 10rem;
-  text-align: center;
+.account-setup {
+  outline: solid 0.063rem $app-grey;
+  border-radius: 0.5rem;
 }
-.p-progress-spinner {
-  display: flex;
-  margin-top: 4rem;
-  position: relative;
+
+.card {
+  width: 28.75rem;
+  height: 48.25rem;
 }
 </style>
