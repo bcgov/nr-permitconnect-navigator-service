13755880e78412558d1da7f58e76f02901a4f6bb
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-04-14 13:45:43 -0700

    Enquiries only return results for set initiative

--- app/src/types/EnquirySearchParameters.ts
+++ app/src/types/EnquirySearchParameters.ts
@@ -1,7 +1,10 @@
+import { Initiative } from '../utils/enums/application';
+
 export type EnquirySearchParameters = {
   activityId?: Array<string>;
   createdBy?: Array<string>;
   enquiryId?: Array<string>;
   intakeStatus?: Array<string>;
   includeUser?: boolean;
+  initiative?: Array<Initiative>;
 };
