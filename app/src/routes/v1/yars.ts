94e8f2ea090712bdf6c125a94b369e2196c49f68
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-04-14 13:43:43 -0700

    Add YARS resource

--- app/src/routes/v1/yars.ts
+++ app/src/routes/v1/yars.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import { yarsController } from '../../controllers';
+import { hasAuthorization } from '../../middleware/authorization';
 import { requireSomeAuth } from '../../middleware/requireSomeAuth';
 import { requireSomeGroup } from '../../middleware/requireSomeGroup';
-import { GroupName } from '../../utils/enums/application';
+import { Action, GroupName, Resource } from '../../utils/enums/application';
 
 import type { NextFunction, Request, Response } from 'express';
 
@@ -11,16 +12,22 @@ const router = express.Router();
 router.use(requireSomeAuth);
 router.use(requireSomeGroup);
 
-router.get('/groups', (req: Request, res: Response, next: NextFunction): void => {
-  yarsController.getGroups(req, res, next);
-});
+router.get(
+  '/groups',
+  hasAuthorization(Resource.YARS, Action.READ),
+  (req: Request, res: Response, next: NextFunction): void => {
+    yarsController.getGroups(req, res, next);
+  }
+);
 
+// Publicly accessible
 router.get('/permissions', (req: Request, res: Response, next: NextFunction): void => {
   yarsController.getPermissions(req, res, next);
 });
 
 router.delete(
   '/subject/group',
+  hasAuthorization(Resource.YARS, Action.DELETE),
   (req: Request<never, never, { sub: string; group: GroupName }>, res: Response, next: NextFunction): void => {
     yarsController.deleteSubjectGroup(req, res, next);
   }
