eb1cd31133bc313a24d50d95a5985828bcfc02e9
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-05-05 11:19:21 -0700

    i18n updates

--- frontend/tests/unit/components/user/UserCreateModal.spec.ts
+++ frontend/tests/unit/components/user/UserCreateModal.spec.ts
@@ -1,14 +1,24 @@
-import UserCreateModal from '@/components/user/UserCreateModal.vue';
+import type { AxiosResponse } from 'axios';
 import { createTestingPinia } from '@pinia/testing';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 import { mount } from '@vue/test-utils';
-import type { AxiosResponse } from 'axios';
+
+import UserCreateModal from '@/components/user/UserCreateModal.vue';
 import { yarsService } from '@/services';
 
+// Mock dependencies
+vi.mock('vue-i18n', () => ({
+  useI18n: () => ({
+    t: vi.fn()
+  })
+}));
+
+// Spies
 const getGroups = vi.spyOn(yarsService, 'getGroups');
 
+// Mocks
 getGroups.mockResolvedValue({ data: [{ activityId: 'someActivityid' }] } as AxiosResponse);
 
 const wrapperSettings = () => ({
