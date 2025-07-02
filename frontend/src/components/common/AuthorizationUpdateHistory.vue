63ac5f074052adcdf0cd8ddbf27691931f3814e3
Sanjay Babu <sanjaytkbabu@gmail.com>
2025-07-02 14:14:31 -0700

    FE: New components Authorization - View, Form, CardIntake, StatusUpdatesCard added, PermitCard, PermitModal removed, Internal ProjectViews to updated to use new components

---
+++ frontend/src/components/common/AuthorizationUpdateHistory.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+// Composables
+const { t } = useI18n();
+</script>
+
+<template>
+  <div class="gap-x-6 gap-y-6 update-history px-5 pt-5 mt-4 py-5">
+    <h5>{{ t('i.common.authorization.authorizationForm.updateNoteHistory') }}</h5>
+  </div>
+</template>
+<style lang="scss" scoped>
+.update-history {
+  background-color: $app-grey;
+  border-radius: 0.5rem;
+}
+</style>
