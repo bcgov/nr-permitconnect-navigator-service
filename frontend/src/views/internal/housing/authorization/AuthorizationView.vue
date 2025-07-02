e606b17436e20e38aed7989c52dde1e14ca4090a
Sanjay Babu <sanjaytkbabu@gmail.com>
2025-07-02 14:14:31 -0700

    FE: New components Authorization - View, Form, CardIntake, StatusUpdatesCard added, PermitCard, PermitModal removed, Internal ProjectViews to updated to use new components

---
+++ frontend/src/views/internal/housing/authorization/AuthorizationView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { provide } from 'vue';
+
+import AuthorizationForm from '@/components/form/common/AuthorizationForm.vue';
+import { housingProjectService } from '@/services';
+import { RouteName } from '@/utils/enums/application';
+import { projectViewRouteNameKey, projectServiceKey } from '@/utils/keys';
+
+const {
+  editable = true,
+  activityId,
+  projectId
+} = defineProps<{
+  editable?: boolean;
+  activityId: string;
+  projectId: string;
+}>();
+
+// Providers
+provide(projectViewRouteNameKey, RouteName.INT_HOUSING_PROJECT);
+provide(projectServiceKey, housingProjectService);
+</script>
+
+<template>
+  <AuthorizationForm
+    :editable="editable"
+    :activity-id="activityId"
+    :project-id="projectId"
+  />
+</template>
