28032cb8d73a8039dc668aa2b51131bcf4d1dfe8
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-07-02 16:02:31 -0700

    Routing/controller/service changes

---
+++ app/src/types/NoteHistory.ts
@@ -0,0 +1,14 @@
+import { IStamps } from '../interfaces/IStamps';
+
+export type NoteHistory = {
+  noteHistoryId?: string; // Primary Key
+  activityId: string;
+  bringForwardDate: string | null;
+  bringForwardState: string | null;
+  escalateToDirector: boolean;
+  escalateToSupervisor: boolean;
+  shownToProponent: boolean;
+  isDeleted: boolean;
+  title: string;
+  type: string;
+} & Partial<IStamps>;
