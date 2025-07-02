be53019c4661de499ea00b6ef206d792ef679649
Sanjay Babu <sanjaytkbabu@gmail.com>
2025-07-02 14:41:21 -0700

    BE & FE tests updated, PermitModal, PermitNotesModal tests removed

---
+++ frontend/tests/unit/views/internal/housing/authorization/AuthorizationView.spec.ts
@@ -0,0 +1,52 @@
+import { createTestingPinia } from '@pinia/testing';
+import PrimeVue from 'primevue/config';
+import { shallowMount } from '@vue/test-utils';
+
+import AuthorizationView from '@/views/internal/housing/authorization/AuthorizationView.vue';
+
+// Mock dependencies
+vi.mock('vue-i18n', () => ({
+  useI18n: () => ({
+    t: vi.fn()
+  })
+}));
+
+vi.mock('vue-router', () => ({
+  useRouter: () => ({
+    push: vi.fn()
+  })
+}));
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
+
+afterEach(() => {
+  sessionStorage.clear();
+});
+
+const wrapperSettings = () => ({
+  global: {
+    plugins: [
+      () =>
+        createTestingPinia({
+          initialState: {
+            auth: {
+              user: {}
+            }
+          }
+        }),
+      PrimeVue
+    ],
+    stubs: ['font-awesome-icon', 'router-link']
+  }
+});
+
+// Tests
+describe('AuthorizationView.vue', () => {
+  it('renders', () => {
+    const wrapper = shallowMount(AuthorizationView, wrapperSettings());
+
+    expect(wrapper).toBeTruthy();
+  });
+});
