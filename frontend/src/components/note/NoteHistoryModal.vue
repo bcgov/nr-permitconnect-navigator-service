a7954952ee26cc1e246c9df1a6adeaacfdffdec0
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-07-10 10:20:05 -0700

    Add remaining data changes for BF escalations

--- frontend/src/components/note/NoteHistoryModal.vue
+++ frontend/src/components/note/NoteHistoryModal.vue
@@ -168,7 +168,7 @@ function onTypeChange(e: SelectChangeEvent) {
 
     <Form
       ref="formRef"
-      v-slot="{ handleReset }"
+      v-slot="{ handleReset, values }"
       :initial-values="initialFormValues"
       :validation-schema="formSchema"
       @submit="onSubmit"
