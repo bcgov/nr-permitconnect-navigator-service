a4c3b99615202a6c56c3376be2e3a5cf9fda6b12
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-07-10 10:20:05 -0700

    Add remaining data changes for BF escalations

--- app/src/utils/constants/projectCommon.ts
+++ app/src/utils/constants/projectCommon.ts
@@ -3,6 +3,7 @@ import {
   BringForwardType,
   ContactPreference,
   EnquirySubmittedMethod,
+  EscalationType,
   IntakeStatus,
   NoteType,
   ProjectRelationship,
@@ -36,6 +37,13 @@ export const ENQUIRY_TYPE_LIST = [
   SubmissionType.STATUS_REQUEST,
   SubmissionType.ESCALATION,
   SubmissionType.INAPPLICABLE
+];
+
+export const ESCALATION_TYPE_LIST = [
+  EscalationType.PROPONENT,
+  EscalationType.MINISTRY,
+  EscalationType.TIME_SENSITIVE,
+  EscalationType.OTHER
 ];
 
 export const INTAKE_STATUS_LIST = [IntakeStatus.SUBMITTED, IntakeStatus.ASSIGNED, IntakeStatus.COMPLETED];
