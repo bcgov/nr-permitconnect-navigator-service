cc4281abaff9a664bda7d4504aeb8f24c3eb1f12
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-07-10 22:50:44 -0700

    Improved naming for some services and functions

--- frontend/tests/unit/views/internal/electrification/project/ProjectView.spec.ts
+++ frontend/tests/unit/views/internal/electrification/project/ProjectView.spec.ts
@@ -4,7 +4,13 @@ import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 import { shallowMount } from '@vue/test-utils';
 
-import { documentService, enquiryService, electrificationProjectService, noteService, permitService } from '@/services';
+import {
+  documentService,
+  enquiryService,
+  electrificationProjectService,
+  noteHistoryService,
+  permitService
+} from '@/services';
 import ProjectView from '@/views/internal/electrification/project/ProjectView.vue';
 
 import type { AxiosResponse } from 'axios';
@@ -26,7 +32,7 @@ vi.mock('vue-router', () => ({
 
 const getProjectSpy = vi.spyOn(electrificationProjectService, 'getProject');
 const listDocumentsSpy = vi.spyOn(documentService, 'listDocuments');
-const listNotesSpy = vi.spyOn(noteService, 'listNoteHistory');
+const listNotesSpy = vi.spyOn(noteHistoryService, 'listNoteHistories');
 const listPermitsSpy = vi.spyOn(permitService, 'listPermits');
 const listRelatedEnquiriesSpy = vi.spyOn(enquiryService, 'listRelatedEnquiries');
 const getPermitTypesSpy = vi.spyOn(permitService, 'getPermitTypes');
