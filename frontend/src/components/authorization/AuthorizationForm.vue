e9f2278aef4700ca90124f683f19d735f83b8441
Quinn Hanson <57877197+qhanson55@users.noreply.github.com>
2025-07-30 12:09:33 -0700

    Minor comments changes to reflect new logic

--- frontend/src/components/authorization/AuthorizationForm.vue
+++ frontend/src/components/authorization/AuthorizationForm.vue
@@ -75,14 +75,14 @@ async function onSubmit(data: any) {
     const result = (await permitService.upsertPermit({ ...permitData, activityId: activityId })).data;
     projectStore.addPermit(result);
 
-    // Prevent creating notes if the above call fails or if the note is empty
+    // Prevent creating notes and sending an update email if the above call fails or if note is empty
     if (result?.permitId && permitNote?.trim().length > 0) {
       await permitNoteService.createPermitNote({
         permitId: result.permitId,
         note: permitNote
       });
 
-      // Send email to the user if permit is needed
+      // Send email to the user if permit is needed or if permit status is new
       if (data.needed === PermitNeeded.YES || data.status !== PermitStatus.NEW) emailNotification(data);
     }
 
