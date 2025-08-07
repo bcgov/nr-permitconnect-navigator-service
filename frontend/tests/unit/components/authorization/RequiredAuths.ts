059cc37a4e9d9706eca77cba2ae2ca117ea4db0a
Sanjay Babu <sanjaytkbabu@gmail.com>
2025-08-07 16:23:26 -0700

    Basic info validation

---
+++ frontend/tests/unit/components/authorization/RequiredAuths.ts
@@ -0,0 +1,73 @@
+import { createTestingPinia } from '@pinia/testing';
+import { shallowMount } from '@vue/test-utils';
+
+import RequiredAuths from '@/components/authorization/RequiredAuths.vue';
+import { StorageKey } from '@/utils/enums/application';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+import Tooltip from 'primevue/tooltip';
+
+vi.mock('vue-i18n', () => ({
+  useI18n: () => ({
+    t: vi.fn()
+  })
+}));
+
+vi.mock('vue-router', () => ({
+  useRouter: () => ({
+    push: vi.fn(),
+    replace: vi.fn()
+  })
+}));
+
+const wrapperSettings = () => ({
+  props: {
+    authsNeeded: [],
+    authsNotNeeded: []
+  },
+  global: {
+    plugins: [
+      () =>
+        createTestingPinia({
+          initialState: {
+            auth: {
+              user: {}
+            }
+          }
+        }),
+      PrimeVue,
+      ConfirmationService,
+      ToastService
+    ],
+    stubs: ['font-awesome-icon'],
+    directives: {
+      Tooltip: Tooltip
+    }
+  }
+});
+
+beforeEach(() => {
+  sessionStorage.setItem(
+    StorageKey.CONFIG,
+    JSON.stringify({
+      oidc: {
+        authority: 'abc',
+        clientId: '123'
+      }
+    })
+  );
+
+  vi.clearAllMocks();
+});
+
+afterEach(() => {
+  sessionStorage.clear();
+});
+
+describe('RequiredAuths', () => {
+  it('renders component', async () => {
+    const wrapper = shallowMount(RequiredAuths, wrapperSettings());
+    expect(wrapper).toBeTruthy();
+  });
+});
