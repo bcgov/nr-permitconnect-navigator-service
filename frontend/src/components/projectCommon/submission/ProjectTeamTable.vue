18888cd562dae9c61facb5522d777af314e4346f
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-11-13 13:28:39 -0800

    Add new components for viewing Project Teams in proponent view

---
+++ frontend/src/components/projectCommon/submission/ProjectTeamTable.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from 'vue';
+
+import { Button, Column, DataTable } from '@/lib/primevue';
+import { useContactStore } from '@/store';
+import { ActivityContactRole } from '@/utils/enums/projectCommon';
+
+import type { Ref } from 'vue';
+import type { ActivityContact } from '@/types';
+
+// Props
+const { activityContacts } = defineProps<{
+  activityContacts: ActivityContact[];
+}>();
+
+// State
+const isAdmin: Ref<boolean> = ref(false);
+
+// Actions
+onBeforeMount(async () => {
+  // Determine if the current user has admin priviledges
+  const userActivityRole = activityContacts.find((x) => x.contactId === useContactStore().getContact?.contactId)?.role;
+  if (userActivityRole)
+    isAdmin.value = [ActivityContactRole.PRIMARY, ActivityContactRole.ADMIN].includes(userActivityRole);
+});
+</script>
+
+<template>
+  <DataTable
+    :value="activityContacts"
+    class="datatable"
+    removable-sort
+  >
+    <template #empty>
+      <div class="flex justify-center">
+        <h5 class="m-0">No users found.</h5>
+      </div>
+    </template>
+    <Column
+      field="contact.firstName"
+      header="First Name"
+      sortable
+    />
+    <Column
+      field="contact.lastName"
+      header="Last Name"
+      sortable
+    />
+    <Column
+      field="contact.email"
+      header="Email"
+      sortable
+    />
+    <Column
+      field="contact.phoneNumber"
+      header="Phone"
+      sortable
+    />
+    <Column
+      field="role"
+      header="Role"
+      sortable
+    />
+    <Column
+      v-if="isAdmin"
+      field="manage"
+      header="Manage"
+      header-class="header-right"
+      class="!text-right"
+    >
+      <template #body="{ data }">
+        <Button
+          :disabled="data.role === ActivityContactRole.PRIMARY"
+          class="p-button-lg p-button-text p-0"
+          aria-label="Manage"
+        >
+          <font-awesome-icon icon="fa-solid fa-pen-to-square" />
+        </Button>
+      </template>
+    </Column>
+    <Column
+      v-if="isAdmin"
+      field="revoke"
+      header="Revoke"
+      header-class="header-right"
+      class="!text-right"
+    >
+      <template #body="{ data }">
+        <Button
+          :disabled="data.role === ActivityContactRole.PRIMARY"
+          class="p-button-lg p-button-text p-button-danger p-0"
+          aria-label="Revoke"
+        >
+          <font-awesome-icon icon="fa-solid fa-user-xmark" />
+        </Button>
+      </template>
+    </Column>
+  </DataTable>
+</template>
--- frontend/src/types/ActivityContact.ts
+++ frontend/src/types/ActivityContact.ts
@@ -1,9 +1,11 @@
 import type { Contact } from './Contact';
 import type { IStamps } from '@/interfaces';
+import type { ActivityContactRole } from '@/utils/enums/projectCommon';
 
 export type ActivityContact = {
   activityId: string;
   contactId: string;
+  role: ActivityContactRole;
 
   // Joined
   contact?: Contact;
