51ec60b978947feeaed4d2a9f523e947cc579a35
qhanson55 <quinn.hanson@gov.bc.ca>
2025-12-02 15:38:02 -0800

    Added missing electrification views tests
    Signed-off-by: qhanson55 <quinn.hanson@gov.bc.ca>

---
+++ frontend/tests/unit/views/external/electrification/enquiry/EnquiryConfirmationView.spec.ts
@@ -0,0 +1,58 @@
+import { createTestingPinia } from '@pinia/testing';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+import { mount } from '@vue/test-utils';
+
+import { userService } from '@/services';
+import EnquiryConfirmationView from '@/views/external/electrification/enquiry/EnquiryConfirmationView.vue';
+
+import type { AxiosResponse } from 'axios';
+
+const useUserService = vi.spyOn(userService, 'searchUsers');
+
+useUserService.mockResolvedValue({ data: [{ fullName: 'dummyName' }] } as AxiosResponse);
+
+const testEnquiryId = 'enquiry123';
+const testActivityId = 'activity123';
+
+vi.mock('vue-i18n', () => ({
+  useI18n: () => ({
+    t: vi.fn()
+  })
+}));
+
+const wrapperSettings = (testEnquiryIdProp = testEnquiryId, testActivityIdProp = testActivityId) => ({
+  props: {
+    enquiryId: testEnquiryIdProp,
+    activityId: testActivityIdProp,
+    showEnquiryLink: ''
+  },
+  global: {
+    plugins: [
+      () =>
+        createTestingPinia({
+          initialState: {
+            auth: {
+              user: {}
+            }
+          }
+        }),
+      PrimeVue,
+      ConfirmationService,
+      ToastService
+    ],
+    stubs: ['font-awesome-icon', 'router-link']
+  }
+});
+
+describe('EnquiryConfirmationView.vue', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('renders the component with the provided props', () => {
+    const wrapper = mount(EnquiryConfirmationView, wrapperSettings());
+    expect(wrapper).toBeTruthy();
+  });
+});
---
+++ frontend/tests/unit/views/external/electrification/project/ProjectView.spec.ts
@@ -0,0 +1,128 @@
+import { createTestingPinia } from '@pinia/testing';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+import { StorageKey } from '@/utils/enums/application';
+import { shallowMount } from '@vue/test-utils';
+
+import {
+  activityContactService,
+  contactService,
+  enquiryService,
+  permitService,
+  electrificationProjectService
+} from '@/services';
+import ProjectView from '@/views/external/electrification/project/ProjectView.vue';
+
+import type { AxiosResponse } from 'axios';
+
+vi.mock('vue-i18n', () => ({
+  useI18n: () => ({
+    t: vi.fn()
+  })
+}));
+
+vi.mock('vue-router', () => ({
+  useRoute: vi.fn(() => ({
+    query: {}
+  })),
+  useRouter: vi.fn(() => ({
+    push: vi.fn(),
+    replace: vi.fn()
+  }))
+}));
+
+beforeEach(() => {
+  sessionStorage.setItem(
+    StorageKey.CONFIG,
+    JSON.stringify({
+      oidc: {
+        authority: 'abc',
+        clientId: '123'
+      }
+    })
+  );
+
+  vi.clearAllMocks();
+});
+
+afterEach(() => {
+  sessionStorage.clear();
+});
+
+const testElectrificationProjectId = 'submission123';
+const exampleContact = {
+  contactId: 'contact123',
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+  phone: '123-456-7890'
+};
+
+const wrapperSettings = (projectId = testElectrificationProjectId) => ({
+  props: {
+    projectId: projectId
+  },
+  global: {
+    plugins: [
+      () =>
+        createTestingPinia({
+          initialState: {
+            auth: {
+              user: {}
+            }
+          }
+        }),
+      PrimeVue,
+      ConfirmationService,
+      ToastService
+    ],
+    stubs: ['font-awesome-icon', 'router-link']
+  }
+});
+
+describe('ProjectView.vue', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('renders the component with the provided props', () => {
+    const listActivityContactsSpy = vi.spyOn(activityContactService, 'listActivityContacts');
+    const listRelatedEnquiriesSpy = vi.spyOn(enquiryService, 'listRelatedEnquiries');
+    const listPermitsSpy = vi.spyOn(permitService, 'listPermits');
+    const getPermitTypesSpy = vi.spyOn(permitService, 'getPermitTypes');
+    const getProjectSpy = vi.spyOn(electrificationProjectService, 'getProject');
+    const searchContactSpy = vi.spyOn(contactService, 'searchContacts');
+    const searchProjectsSpy = vi.spyOn(electrificationProjectService, 'searchProjects');
+
+    listActivityContactsSpy.mockResolvedValue({
+      data: []
+    } as AxiosResponse);
+
+    listRelatedEnquiriesSpy.mockResolvedValue({
+      data: 'notTested'
+    } as AxiosResponse);
+
+    listPermitsSpy.mockResolvedValue({
+      data: 'notTested'
+    } as AxiosResponse);
+
+    getPermitTypesSpy.mockResolvedValue({
+      data: 'notTested'
+    } as AxiosResponse);
+
+    getProjectSpy.mockResolvedValue({
+      data: 'notTested'
+    } as AxiosResponse);
+
+    searchContactSpy.mockResolvedValue({
+      data: [exampleContact]
+    } as AxiosResponse);
+
+    searchProjectsSpy.mockResolvedValue({
+      data: [{ activityId: 'activity456' }]
+    } as AxiosResponse);
+
+    const wrapper = shallowMount(ProjectView, wrapperSettings());
+    expect(wrapper).toBeTruthy();
+  });
+});
