28b1831b6945ba9dce7da4ad443661091609c79b
Sanjay Babu <sanjaytkbabu@gmail.com>
2025-12-11 10:11:24 -0800

    Permit type source system kind filter and their mapping table added
    Signed-off-by: Sanjay Babu <sanjaytkbabu@gmail.com>

--- app/src/services/sourceSystemKind.ts
+++ app/src/services/sourceSystemKind.ts
@@ -3,16 +3,32 @@ import type { PrismaTransactionClient } from '../db/dataConnection';
 import type { SourceSystemKind } from '../types';
 
 /**
- * Get all source system kinds
+ * Get all source system kinds and include their associated permit type ids
  * @param tx Prisma transaction client
- * @returns A Promise that resolves to an array of source system kinds
+ * @returns A Promise that resolves to an array of source system kinds along with their permit type ids
  */
 export const getSourceSystemKinds = async (tx: PrismaTransactionClient): Promise<SourceSystemKind[]> => {
   const response = await tx.source_system_kind.findMany({
     orderBy: {
       sourceSystem: 'asc'
+    },
+    include: {
+      permitTypeSourceSystemKind: {
+        select: {
+          permitTypeId: true
+        }
+      }
     }
   });
 
-  return response;
+  // Transform the response to flatten permitTypeSourceSystemKind to an array of permit type IDs and rename the field
+  const transformedResponse = response.map((item) => {
+    const { permitTypeSourceSystemKind, ...rest } = item;
+    return {
+      ...rest,
+      permitTypeIds: permitTypeSourceSystemKind?.map((pt) => pt.permitTypeId)
+    };
+  });
+
+  return transformedResponse;
 };
