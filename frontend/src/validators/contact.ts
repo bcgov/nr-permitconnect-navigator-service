61625f4782b845fa935b2210e0854225fd7115f1
qhanson55 <quinn.hanson@gov.bc.ca>
2026-03-10 08:59:11 -0700

    fix(activity-contact): Change contact validator to match actual db fields
    - Removed redundant "contact" in contact fields.
    - Fixed tests
    - Only minimal fix done, full fix coming with new General Form release branch
    
    Signed-off-by: qhanson55 <quinn.hanson@gov.bc.ca>

--- frontend/src/validators/contact.ts
+++ frontend/src/validators/contact.ts
@@ -4,12 +4,12 @@ import { CONTACT_PREFERENCE_LIST, PROJECT_RELATIONSHIP_LIST } from '@/utils/cons
 import { emailValidator } from '@/validators/common';
 
 const contact = {
-  contactPreference: string().required().oneOf(CONTACT_PREFERENCE_LIST).label('Preferred contact method'),
-  contactEmail: emailValidator('Contact email must be valid').required().label('Contact email'),
-  contactFirstName: string().required().max(255).label('Contact first name'),
-  contactLastName: string().max(255).label('Contact last name').nullable(),
-  contactPhoneNumber: string().required().label('Contact phone number'),
-  contactApplicantRelationship: string().required().oneOf(PROJECT_RELATIONSHIP_LIST).label('Relationship to project')
+  email: emailValidator('Contact email must be valid').required().label('Contact email'),
+  firstName: string().required().max(255).label('Contact first name'),
+  lastName: string().max(255).label('Contact last name'),
+  phoneNumber: string().required().label('Contact phone number'),
+  contactApplicantRelationship: string().required().oneOf(PROJECT_RELATIONSHIP_LIST).label('Relationship to project'),
+  contactPreference: string().required().oneOf(CONTACT_PREFERENCE_LIST).label('Preferred contact method')
 };
 
 export default contact;
