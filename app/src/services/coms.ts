69be3f36125c3c2db8f963051b18d4686825d9e2
Kyle Morel <kyle.1.morel@gov.bc.ca>
2026-03-16 17:01:08 -0700

    chore: update COMS service
    Use pseudo service account where required. Update how COMS permissions are applied for external users.

--- app/src/services/coms.ts
+++ app/src/services/coms.ts
@@ -1,17 +1,33 @@
 import axios from 'axios';
 import config from 'config';
 
-import { Action } from '../utils/enums/application.ts';
-import { Problem, uuidValidateV4 } from '../utils/index.ts';
+import { Action, GroupName } from '../utils/enums/application.ts';
+import { getCurrentSubject, Problem, uuidValidateV4 } from '../utils/index.ts';
+import { getSubjectGroups } from './yars.ts';
 
 import type { AxiosInstance, AxiosRequestConfig } from 'axios';
+import type { PrismaTransactionClient } from '../db/dataConnection.ts';
+import type { CurrentContext } from '../types/stuff';
 
 /**
  * Returns an Axios instance for the COMS API
+ * Injects pseudo service account basic auth if no other auth provided
  * @param options Axios request config options
  * @returns An axios instance
  */
 function comsAxios(options: AxiosRequestConfig = {}): AxiosInstance {
+  // Inject pseudo service account if no other auth provided
+  if (!options.headers?.Authorization) {
+    const ak = config.get<string>('server.objectStorage.accessKeyId');
+    const sak = config.get<string>('server.objectStorage.secretAccessKey');
+    const b64 = Buffer.from(ak + ':' + sak).toString('base64');
+
+    if (!options.headers) options.headers = {};
+    options.headers.Authorization = `basic ${b64}`;
+    options.headers['x-amz-bucket'] = config.get('server.objectStorage.bucket');
+    options.headers['x-amz-endpoint'] = config.get('server.objectStorage.endpoint');
+  }
+
   // Create axios instance
   const instance = axios.create({
     baseURL: config.get('frontend.coms.apiPath'),
@@ -23,27 +39,20 @@ function comsAxios(options: AxiosRequestConfig = {}): AxiosInstance {
 }
 
 /**
- * Creates a bucket record. Bucket should exist in S3. If the set of bucket, endpoint and key match
- * an existing record, the user will be added to that existing bucket with the provided permissions
- * instead of generating a new bucket record.
- * This endpoint can be used to grant the current user permission to upload to a new or existing bucket.
- * @param bearerToken The bearer token of the authorized user
- * @param permissions An array of permissions to grant the user
+ * Obtain a bucket record. This actually utilizes the COMS create bucket endpoint as its the only way to
+ * obtain full bucket information using their pseudo service account access.
  * @returns The created bucket data
  */
-export const createBucket = async (bearerToken: string, permissions: Action[]) => {
-  const { data } = await comsAxios({
-    headers: { Authorization: `Bearer ${bearerToken}` }
-  }).put('/bucket', {
+export const getBucket = async () => {
+  const { status, headers, data } = await comsAxios().put('/bucket', {
     accessKeyId: config.get('server.objectStorage.accessKeyId'),
     bucket: config.get('server.objectStorage.bucket'),
     bucketName: 'PCNS',
     endpoint: config.get('server.objectStorage.endpoint'),
     secretAccessKey: config.get('server.objectStorage.secretAccessKey'),
-    key: config.get('server.objectStorage.key'),
-    permCodes: permissions
+    key: config.get('server.objectStorage.key')
   });
-  return data;
+  return { status, headers, data };
 };
 
 /**
@@ -61,4 +70,72 @@ export const getObject = async (bearerToken: string, objectId: string) => {
     headers: { Authorization: `Bearer ${bearerToken}` }
   }).get(`/object/${objectId}`);
   return { status, headers, data };
+};
+
+/**
+ * Obtain the current user information in COMS
+ * @param currentContext The current context of the Express request
+ * @returns The COMS response
+ */
+export const searchUser = async (currentContext: CurrentContext) => {
+  const { status, headers, data } = await comsAxios({
+    headers: { Authorization: `Bearer ${currentContext.bearerToken}` }
+    // Update to use username (sub) if COMS ever opens that up
+  }).get('/user', { params: { identityId: getCurrentSubject(currentContext).split('@')[0].toUpperCase() } });
+  return { status, headers, data };
+};
+
+/**
+ * Assigns COMS permissions to the current user based on their current groups
+ * @param tx Prisma transaction client
+ * @param currentContext The current context of the Express request
+ */
+export const assignPermissions = async (tx: PrismaTransactionClient, currentContext: CurrentContext) => {
+  const sub = currentContext.tokenPayload?.sub;
+
+  if (!sub) {
+    throw new Problem(403, {
+      detail: 'Unable to obtain token sub'
+    });
+  }
+
+  const groups = await getSubjectGroups(tx, sub);
+  const groupNames = new Set(groups.map((x) => x.name));
+
+  const comsPermsMap = new Map<GroupName, Action[]>([
+    [GroupName.PROPONENT, [Action.CREATE]],
+    [GroupName.NAVIGATOR_READ_ONLY, [Action.READ]],
+    [GroupName.NAVIGATOR, [Action.CREATE, Action.READ, Action.UPDATE, Action.DELETE]],
+    [GroupName.SUPERVISOR, [Action.CREATE, Action.READ, Action.UPDATE, Action.DELETE]],
+    [GroupName.ADMIN, [Action.CREATE, Action.READ, Action.UPDATE, Action.DELETE]],
+    [GroupName.DEVELOPER, [Action.CREATE, Action.READ, Action.UPDATE, Action.DELETE]]
+  ]);
+
+  const actions = new Set<Action>([...groupNames].flatMap((groupName) => comsPermsMap.get(groupName) ?? []));
+
+  if (actions && currentContext) {
+    try {
+      const user = await searchUser(currentContext);
+      const bucket = await getBucket();
+
+      const { userId } = user.data[0];
+      const { bucketId } = bucket.data;
+
+      if (!userId || !bucketId) {
+        throw new Error('Unable to obtain userId or bucketId');
+      }
+
+      const permissions = [...actions].flatMap((action) => ({ permCode: action, userId }));
+
+      await comsAxios().put(`/permission/bucket/${bucketId}`, permissions);
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        const detail = e.response?.data.detail;
+        const status = e.response ? e.response.status : 500;
+        throw new Problem(status, { detail }, { extra: { comsError: e.response?.data } });
+      } else {
+        throw new Problem(500, { detail: 'Server Error' });
+      }
+    }
+  }
 };
