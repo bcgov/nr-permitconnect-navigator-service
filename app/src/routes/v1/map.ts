f549b14413309565088ba456b04f494766c10a44
Kyle Morel <kyle.1.morel@gov.bc.ca>
2026-03-23 16:29:09 -0700

    feat(api): enable PID lookup for multiple initiatives

--- app/src/routes/v1/map.ts
+++ app/src/routes/v1/map.ts
@@ -4,13 +4,30 @@ import { getPIDsController } from '../../controllers/map.ts';
 import { hasAuthorization } from '../../middleware/authorization.ts';
 import { requireSomeAuth } from '../../middleware/requireSomeAuth.ts';
 import { requireSomeGroup } from '../../middleware/requireSomeGroup.ts';
-import { Action, Resource } from '../../utils/enums/application.ts';
+import { Action, Initiative, Resource } from '../../utils/enums/application.ts';
+
+import type { NextFunction, Request, Response } from 'express';
 
 const router = express.Router();
 router.use(requireSomeAuth);
 router.use(requireSomeGroup);
 
+const INITIATIVE_RESOURCE_MAP = new Map<Initiative, Resource>([
+  [Initiative.GENERAL, Resource.GENERAL_PROJECT],
+  [Initiative.HOUSING, Resource.HOUSING_PROJECT]
+]);
+
 /** Get PIDs for a housing project */
-router.get('/pids/:projectId', hasAuthorization(Resource.HOUSING_PROJECT, Action.READ), getPIDsController);
+router.get(
+  '/pids/:projectId',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const r = INITIATIVE_RESOURCE_MAP.get(req.currentContext.initiative);
+    if (!r) {
+      throw new Error('No resource');
+    }
+    return hasAuthorization(r, Action.READ)(req, res, next);
+  },
+  getPIDsController
+);
 
 export default router;
