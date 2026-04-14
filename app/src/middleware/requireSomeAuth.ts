5faae3ac812c5d1bbe3827c1b6879c18d6ece678
qhanson55 <quinn.hanson@gov.bc.ca>
2026-04-14 09:21:07 -0700

    feat(auth): implement robust OIDC authentication and caching
    Refactors authentication middleware to support dynamic JWKS verification and adds an LRU cache for JWT payloads. This change validates tokens against provider public keys and reduces redundant verification, database lookups, and the need to store/define public keys.
    
    - Update authentication middleware, changed `currentContext` to `hasAuthentication`, and improve flow and error handling.
    - Add `LRUCache` for `JwtPayload` with TTL based on expiration.
    - Add `providers/oidc.ts` for RFC 6750 header parsing, error handling, and implement JWKS retrieval with caching and request locking.
    - Update `requireSomeAuth` to return 401 for missing authentication.
    - Add unit tests for middleware and oidc providers.
    - Install cross-env, change npm test command, and added vscode jest setting to allow for dynamic importing with node options flag `--experimental-vm-modules`.
    
    Signed-off-by: qhanson55 <quinn.hanson@gov.bc.ca>

--- app/src/middleware/requireSomeAuth.ts
+++ app/src/middleware/requireSomeAuth.ts
@@ -1,3 +1,6 @@
+import config from 'config';
+
+import { setAuthHeader } from './providers/oidc.ts';
 import { Problem } from '../utils/index.ts';
 import { AuthType } from '../utils/enums/application.ts';
 
@@ -8,16 +11,16 @@ import type { NextFunction, Request, Response } from 'express';
  * @param req Express request object
  * @param res Express response object
  * @param next The next callback function
- * @throws {Problem} The error encountered upon failure
+ * @returns Express middleware next function.
  */
 export const requireSomeAuth = (req: Request, res: Response, next: NextFunction) => {
   const authType: string | undefined = req.currentContext ? req.currentContext.authType : undefined;
 
   if (!authType || authType === AuthType.NONE) {
-    throw new Problem(403, {
-      detail: 'User lacks permission to complete this action',
-      instance: req.originalUrl
-    });
+    const audience: string = config.get('server.oidc.audience');
+    setAuthHeader(res, { realm: audience });
+    // Problem is a 401 due to missing authentication as all other errors are handled in authentication middleware
+    return next(new Problem(401, { detail: 'Missing authentication', instance: req.originalUrl }));
   }
 
   next();
