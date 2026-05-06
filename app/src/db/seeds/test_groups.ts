29d8d3687ec11133bd882ddc761e8b2c2d74a9fd
Kyle Morel <kyle.1.morel@gov.bc.ca>
2026-05-06 12:16:28 -0700

    fix(db): update dev/test/prod rbac group seeds

--- app/src/db/seeds/test_groups.ts
+++ app/src/db/seeds/test_groups.ts
@@ -27,22 +27,22 @@ export async function seed(knex: Knex): Promise<void> {
     .select('group_id');
 
   const identities = [
-    { sub: '3dcfc748ac7a45bfa98630e44710653c@idir', group_id: admin_group_id[0].group_id },
+    { sub: '3dcfc748ac7a45bfa98630e44710653c@azureidir', group_id: admin_group_id[0].group_id },
 
-    { sub: '8895e3992ee9455b8eef3ab435dd5486@idir', group_id: developer_group_id[0].group_id },
-    { sub: '17222a61118846b08bdda2769a585278@idir', group_id: developer_group_id[0].group_id },
-    { sub: 'a64c23b15d964d9b979188f356e083ea@idir', group_id: developer_group_id[0].group_id },
-    { sub: '333c6b2f38454628b638ca4ac52eab68@idir', group_id: developer_group_id[0].group_id },
-    { sub: 'e268cd73d4b544d4bc2c978fb98f342a@idir', group_id: developer_group_id[0].group_id },
-    { sub: 'e7d30762682b437cb8d60502c956a516@idir', group_id: developer_group_id[0].group_id },
-    { sub: 'd79fb88e50414ca789620bb89915b2e5@idir', group_id: developer_group_id[0].group_id },
-    { sub: '4a07a001034549d79f525715378e617a@idir', group_id: developer_group_id[0].group_id },
+    { sub: '8895e3992ee9455b8eef3ab435dd5486@azureidir', group_id: developer_group_id[0].group_id },
+    { sub: '17222a61118846b08bdda2769a585278@azureidir', group_id: developer_group_id[0].group_id },
+    { sub: 'a64c23b15d964d9b979188f356e083ea@azureidir', group_id: developer_group_id[0].group_id },
+    { sub: '333c6b2f38454628b638ca4ac52eab68@azureidir', group_id: developer_group_id[0].group_id },
+    { sub: 'e268cd73d4b544d4bc2c978fb98f342a@azureidir', group_id: developer_group_id[0].group_id },
+    { sub: 'e7d30762682b437cb8d60502c956a516@azureidir', group_id: developer_group_id[0].group_id },
+    { sub: 'd79fb88e50414ca789620bb89915b2e5@azureidir', group_id: developer_group_id[0].group_id },
+    { sub: '4a07a001034549d79f525715378e617a@azureidir', group_id: developer_group_id[0].group_id },
 
-    { sub: '067f5f2385cd43fda8c3cb55b04e992e@idir', group_id: navigator_group_id[0].group_id },
-    { sub: '8bca8dc363494d8383ca5091e6712fdb@idir', group_id: navigator_group_id[0].group_id },
-    { sub: '9ab23d31af1440a0920e69aabba45e22@idir', group_id: navigator_group_id[0].group_id },
-    { sub: 'b50e45b3a01e4a7983ca7da70eae74e2@idir', group_id: navigator_group_id[0].group_id },
-    { sub: 'f3237101e6034ac1bd247342f6d4eb86@idir', group_id: navigator_group_id[0].group_id }
+    { sub: '067f5f2385cd43fda8c3cb55b04e992e@azureidir', group_id: navigator_group_id[0].group_id },
+    { sub: '8bca8dc363494d8383ca5091e6712fdb@azureidir', group_id: navigator_group_id[0].group_id },
+    { sub: '9ab23d31af1440a0920e69aabba45e22@azureidir', group_id: navigator_group_id[0].group_id },
+    { sub: 'b50e45b3a01e4a7983ca7da70eae74e2@azureidir', group_id: navigator_group_id[0].group_id },
+    { sub: 'f3237101e6034ac1bd247342f6d4eb86@azureidir', group_id: navigator_group_id[0].group_id }
   ];
 
   const exists = await knex('yars.subject_group').whereIn(
