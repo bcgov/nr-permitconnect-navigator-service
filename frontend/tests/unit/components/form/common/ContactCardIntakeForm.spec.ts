fa17b3bd31f4be2700fb1dc55753f981f968667c
Sanjay Babu <sanjaytkbabu@gmail.com>
2026-09-04 16:53:35 -0700

    refactor: update numerous tests to align with new standards

--- frontend/tests/unit/components/form/common/ContactCardIntakeForm.spec.ts
+++ frontend/tests/unit/components/form/common/ContactCardIntakeForm.spec.ts
@@ -5,6 +5,8 @@ import { useFormStore } from '@/store';
 import { mountWithFormContext } from '../../../../mountWithFormContext';
 import { FormType, FormState } from '@/utils/enums/projectCommon';
 
+// Mount
+
 function mountContactCardIntakeForm(options: { formType?: FormType; formState?: FormState; tab?: number } = {}) {
   const { formType = FormType.NEW, formState = FormState.UNLOCKED, tab } = options;
 
@@ -22,119 +24,120 @@ beforeEach(() => {
   vi.clearAllMocks();
 });
 
+// Tests
+
 describe('ContactCardIntakeForm', () => {
-  it('renders', () => {
-    const { wrapper } = mountContactCardIntakeForm();
-    expect(wrapper).toBeTruthy();
-  });
+  describe('renders', () => {
+    it('renders a non-empty translated header', () => {
+      const { wrapper } = mountContactCardIntakeForm();
 
-  it('renders a non-empty translated header', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+      expect(wrapper.find('h6').text().trim().length).toBeGreaterThan(0);
+    });
+    describe('mandatory fields', () => {
+      describe('firstName', () => {
+        it('renders', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-    expect(wrapper.find('h6').text().trim().length).toBeGreaterThan(0);
-  });
-});
+          const inputTexts = wrapper.findAllComponents(InputText);
+          const firstNameInput = inputTexts.find((input) => input.props('name') === 'contacts.firstName');
 
-describe('renders all mandatory fields', () => {
-  it('renders InputText for firstName with correct name', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+          expect(firstNameInput).toBeTruthy();
+        });
+        it('displays asterisk', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-    const inputTexts = wrapper.findAllComponents(InputText);
-    const firstNameInput = inputTexts.find((input) => input.props('name') === 'contacts.firstName');
+          const labels = wrapper.findAll('label');
+          const firstNameLabel = labels.find((label) => label.attributes('for') === 'contacts.firstName');
+          const asterisk = firstNameLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
 
-    expect(firstNameInput).toBeTruthy();
-  });
-  it('renders InputMask for phoneNumber with correct name and mask', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+          expect(asterisk).toBeTruthy();
+          expect(asterisk?.text()).toBe('*');
+        });
+      });
+      describe('phoneNumber', () => {
+        it('renders', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-    const phoneInput = wrapper.findComponent(InputMask);
+          const phoneInput = wrapper.findComponent(InputMask);
 
-    expect(phoneInput.props('name')).toBe('contacts.phoneNumber');
-    expect(phoneInput.props('mask')).toBe('(999) 999-9999');
-  });
-  it('renders InputText for email with correct name', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+          expect(phoneInput.props('name')).toBe('contacts.phoneNumber');
+          expect(phoneInput.props('mask')).toBe('(999) 999-9999');
+        });
+        it('displays asterisk', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-    const inputTexts = wrapper.findAllComponents(InputText);
-    const emailInput = inputTexts.find((input) => input.props('name') === 'contacts.email');
+          const labels = wrapper.findAll('label');
+          const phoneLabel = labels.find((label) => label.attributes('for') === 'contacts.phoneNumber');
+          const asterisk = phoneLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
 
-    expect(emailInput).toBeTruthy();
-  });
-  it('renders Select for contactApplicantRelationship with correct name', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+          expect(asterisk).toBeTruthy();
+          expect(asterisk?.text()).toBe('*');
+        });
+      });
+      describe('email', () => {
+        it('renders', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-    const selects = wrapper.findAllComponents(Select);
-    const relationshipSelect = selects.find(
-      (select) => select.props('name') === 'contacts.contactApplicantRelationship'
-    );
+          const inputTexts = wrapper.findAllComponents(InputText);
+          const emailInput = inputTexts.find((input) => input.props('name') === 'contacts.email');
 
-    expect(relationshipSelect).toBeTruthy();
-  });
-  it('renders Select for contactPreference with correct name', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+          expect(emailInput).toBeTruthy();
+        });
+        it('displays asterisk', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-    const selects = wrapper.findAllComponents(Select);
-    const preferenceSelect = selects.find((select) => select.props('name') === 'contacts.contactPreference');
+          const labels = wrapper.findAll('label');
+          const emailLabel = labels.find((label) => label.attributes('for') === 'contacts.email');
+          const asterisk = emailLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
 
-    expect(preferenceSelect).toBeTruthy();
-  });
-});
+          expect(asterisk).toBeTruthy();
+          expect(asterisk?.text()).toBe('*');
+        });
+      });
+      describe('contactApplicantRelationship', () => {
+        it('renders', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-describe('required fields with asterisks', () => {
-  it('displays asterisk for firstName field', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+          const selects = wrapper.findAllComponents(Select);
+          const relationshipSelect = selects.find(
+            (select) => select.props('name') === 'contacts.contactApplicantRelationship'
+          );
 
-    const labels = wrapper.findAll('label');
-    const firstNameLabel = labels.find((label) => label.attributes('for') === 'contacts.firstName');
-    const asterisk = firstNameLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
+          expect(relationshipSelect).toBeTruthy();
+        });
+        it('displays asterisk', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-    expect(asterisk).toBeTruthy();
-    expect(asterisk?.text()).toBe('*');
-  });
+          const labels = wrapper.findAll('label');
+          const relationshipLabel = labels.find(
+            (label) => label.attributes('for') === 'contacts.contactApplicantRelationship'
+          );
+          const asterisk = relationshipLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
 
-  it('displays asterisk for phoneNumber field', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+          expect(asterisk).toBeTruthy();
+          expect(asterisk?.text()).toBe('*');
+        });
+      });
+      describe('contactPreference', () => {
+        it('renders', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-    const labels = wrapper.findAll('label');
-    const phoneLabel = labels.find((label) => label.attributes('for') === 'contacts.phoneNumber');
-    const asterisk = phoneLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
+          const selects = wrapper.findAllComponents(Select);
+          const preferenceSelect = selects.find((select) => select.props('name') === 'contacts.contactPreference');
 
-    expect(asterisk).toBeTruthy();
-    expect(asterisk?.text()).toBe('*');
-  });
+          expect(preferenceSelect).toBeTruthy();
+        });
+        it('displays asterisk for contactPreference field', () => {
+          const { wrapper } = mountContactCardIntakeForm();
 
-  it('displays asterisk for email field', () => {
-    const { wrapper } = mountContactCardIntakeForm();
+          const labels = wrapper.findAll('label');
+          const preferenceLabel = labels.find((label) => label.attributes('for') === 'contacts.contactPreference');
+          const asterisk = preferenceLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
 
-    const labels = wrapper.findAll('label');
-    const emailLabel = labels.find((label) => label.attributes('for') === 'contacts.email');
-    const asterisk = emailLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
-
-    expect(asterisk).toBeTruthy();
-    expect(asterisk?.text()).toBe('*');
-  });
-
-  it('displays asterisk for contactApplicantRelationship field', () => {
-    const { wrapper } = mountContactCardIntakeForm();
-
-    const labels = wrapper.findAll('label');
-    const relationshipLabel = labels.find(
-      (label) => label.attributes('for') === 'contacts.contactApplicantRelationship'
-    );
-    const asterisk = relationshipLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
-
-    expect(asterisk).toBeTruthy();
-    expect(asterisk?.text()).toBe('*');
-  });
-
-  it('displays asterisk for contactPreference field', () => {
-    const { wrapper } = mountContactCardIntakeForm();
-
-    const labels = wrapper.findAll('label');
-    const preferenceLabel = labels.find((label) => label.attributes('for') === 'contacts.contactPreference');
-    const asterisk = preferenceLabel?.find('span.text-\\[var\\(--p-support-required-text\\)\\]');
-
-    expect(asterisk).toBeTruthy();
-    expect(asterisk?.text()).toBe('*');
+          expect(asterisk).toBeTruthy();
+          expect(asterisk?.text()).toBe('*');
+        });
+      });
+    });
   });
 });
