3f137b74160a8e760ecabe7b4eae91e82167db17
qhanson55 <quinn.hanson@gov.bc.ca>
2026-09-08 15:17:17 -0700

    fix: added need network policies

--- app/tests/unit/db/database.test.ts
+++ app/tests/unit/db/database.test.ts
@@ -12,7 +12,6 @@ vi.mock('config', () => ({
   }
 }));
 
-// THIS IS ONLY A TEST
 const prismaInternalMock = mockDeep<DatabaseModule.ExtendedClient>();
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (prismaInternalMock as any).$extends = vi.fn().mockReturnValue(prismaInternalMock);
